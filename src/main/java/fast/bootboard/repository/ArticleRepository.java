308447d1ecd2596974553ba568b130cf3249ecfb
renechoi <renechoi90@gmail.com>
2023-03-25 20:51:24 +0900

    feat: article service

--- src/main/java/fast/bootboard/repository/ArticleRepository.java
+++ src/main/java/fast/bootboard/repository/ArticleRepository.java
@@ -4,6 +4,9 @@ import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import fast.bootboard.domain.Article;
 import fast.bootboard.domain.QArticle;
+import fast.bootboard.dto.ArticleDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -16,6 +19,13 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // 이 기능으로 검색기능 자동으로
         QuerydslBinderCustomizer<QArticle> {
 
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+
+    void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
+
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);  // listing 하지 않을 거는 제외 시킴
@@ -27,4 +37,6 @@ public interface ArticleRepository extends
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
+
+    Page<ArticleDto> findByHashTag(String s);
 }
--- src/main/java/fast/bootboard/service/ArticleService.java
+++ src/main/java/fast/bootboard/service/ArticleService.java
@@ -1,13 +1,17 @@
 package fast.bootboard.service;
 
+import fast.bootboard.domain.Article;
 import fast.bootboard.domain.type.SearchType;
 import fast.bootboard.dto.ArticleDto;
-import fast.bootboard.dto.ArticleUpdateDto;
+import fast.bootboard.dto.ArticleWithCommentsDto;
 import fast.bootboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,21 +21,41 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
-        return Page.empty();
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
+       if (searchKeyword== null || searchKeyword.isBlank()){
+           return articleRepository.findAll(pageable).map(ArticleDto::from);
+       }
+
+       return switch (searchType) {
+           case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+           case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+           case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+           case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+           case HASHTAG -> articleRepository.findByHashTag("#"+searchKeyword);
+       };
+
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
-        return null;
+    public ArticleWithCommentsDto searchArticle(long articleId) {
+        return articleRepository.findById(articleId).map(ArticleWithCommentsDto::from)
+                .orElseThrow(()->new EntityNotFoundException("게시글이 없습니다 - articleId:"+ articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(long articleId, ArticleDto dto) {
+        Article article = articleRepository.getReferenceById(dto.id());
+        if (dto.title() != null){ article.setTitle(dto.title());}
+        if (dto.content() != null){ article.setContent(dto.content());}
+        article.setHashtag(dto.hashtag());
+        articleRepository.save(article);        // TODO : update에서 save 방식 ?
+
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
