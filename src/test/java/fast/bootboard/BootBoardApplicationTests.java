5c7c06deceeb9973d88a171fbdcc031ca3de7e7c
renechoi <renechoi90@gmail.com>
2023-03-25 10:41:47 +0900

    Initial commit

---
+++ src/test/java/fast/bootboard/BootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.bootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
