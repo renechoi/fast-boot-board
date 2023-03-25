b27366420c0356b753c2de9c934b2abc125a0bae
renechoi <renechoi90@gmail.com>
2023-03-25 18:21:48 +0900

    test: articles view and controller

---
+++ src/test/java/fast/bootboard/controller/ArticleControllerTest.java
@@ -0,0 +1,60 @@
+package fast.bootboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Disabled
+@DisplayName("view controller")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mockMvc) {
+        this.mvc = mockMvc;
+    }
+
+    @DisplayName("view - get 게시글 리스트 페이지 정상 호출 ")
+    @Test
+    void articleList() throws Exception {
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("view - get 게시글 페이지 정상 호출 ")
+    @Test
+    void article() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @DisplayName("view - get 게시글 검색 페이지 정상 호출 ")
+    @Test
+    void articleSearch() throws Exception {
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("view - get 게시글 리스트 페이지 정상 호출 ")
+    @Test
+    void articleSearchHashtag() throws Exception {
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
