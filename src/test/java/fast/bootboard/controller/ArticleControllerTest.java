cb7504b7b153132145a16c2ac1a388e7222658e2
renechoi <renechoi90@gmail.com>
2023-03-26 09:16:39 +0900

    feat: implement search

--- src/test/java/fast/bootboard/controller/ArticleControllerTest.java
+++ src/test/java/fast/bootboard/controller/ArticleControllerTest.java
@@ -2,6 +2,7 @@ package fast.bootboard.controller;
 
 
 import fast.bootboard.config.SecurityConfig;
+import fast.bootboard.domain.type.SearchType;
 import fast.bootboard.dto.ArticleWithCommentsDto;
 import fast.bootboard.dto.UserAccountDto;
 import fast.bootboard.service.ArticleService;
@@ -107,6 +108,30 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
         then(articleService).should().getArticle(articleId);
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void givenSearchKeyword_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+
+        // When & Then
+        mvc.perform(
+                        get("/articles")
+                                .queryParam("searchType", searchType.name())
+                                .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
     @Disabled("구현 중")
